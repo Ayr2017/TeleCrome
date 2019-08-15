9da8ebe6513a1c36f2871c5c0060278c42cc3509
Ayrat Asfandiyarov <ayrat.2013@ya.ru>
2019-08-15 13:13:29 +0300

    save in file with time

--- tele-script.js
+++ tele-script.js
@@ -1,65 +1,72 @@
 window.onload = function() {
-    let telegrammLink = 'https://web.telegram.org/#/im?p=@steroid_one_en';
-    var loginCollection = new Set();
-    var loginMap = new Map();
-    let members;
-    let loginWrapper;
-    let userName;
-    let userPhone;
-    let chatName;
+        let telegrammLink = 'https://web.telegram.org/#/im?p=@steroid_one_en';
+        var loginCollection = new Set();
+        var loginMap = new Map();
+        let members;
+        let loginWrapper;
+        let userName;
+        let userPhone;
+        let chatName;
+        let timeStart;
+        let timeEnd;
 
-    function KeyPress(e) {
-        var evtobj = window.event ? event : e
-        if (evtobj.keyCode == 90 && evtobj.ctrlKey) {
-            chatName = document.querySelectorAll('.tg_head_peer_title')[0].innerHTML;
-            openMembersList();
-        }
-
-    }
-
-    function saveFile(data, filename, type) {
-        var file = new Blob([data], { type: type });
-        if (window.navigator.msSaveOrOpenBlob) // IE10+
-            window.navigator.msSaveOrOpenBlob(file, filename);
-        else { // Others
-            var a = document.createElement("a"),
-                url = URL.createObjectURL(file);
-            a.href = url;
-            a.download = filename;
-            document.body.appendChild(a);
-            a.click();
-            setTimeout(function() {
-                document.body.removeChild(a);
-                window.URL.revokeObjectURL(url);
-            }, 10);
-        }
-    }
-
-    function openMembersList() {
-        let membersList = document.querySelectorAll('.tg_head_peer_info');
-        if (membersList.length < 1) {
-            setTimeout(function() {
+        function KeyPress(e) {
+            var evtobj = window.event ? event : e
+            if (evtobj.keyCode == 90 && evtobj.ctrlKey) {
+                chatName = document.querySelectorAll('.tg_head_peer_title')[0].innerHTML;
+                timeStart = new Date().getTime();
                 openMembersList();
-            }, 500)
-        } else {
-            console.dir(membersList[0].parentNode);
-            membersList[0].parentNode.click();
-            getMemberLink();
+            }
+
         }
 
-    }
+        function saveFile(data, filename, type) {
+            var file = new Blob([data], { type: type });
+            if (window.navigator.msSaveOrOpenBlob) // IE10+
+                window.navigator.msSaveOrOpenBlob(file, filename);
+            else { // Others
+                var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+                a.href = url;
+                a.download = filename;
+                document.body.appendChild(a);
+                a.click();
+                setTimeout(function() {
+                    document.body.removeChild(a);
+                    window.URL.revokeObjectURL(url);
+                }, 10);
+            }
+        }
 
-    function getMemberLink() {
-        members = document.querySelectorAll('a.md_modal_list_peer_name');
-        if (members.length > 0) {
-
-            if (members.length > loginCollection.size) {
-                members[parseInt(loginCollection.size)].click();
-                getMemberLogin();
+        function openMembersList() {
+            let membersList = document.querySelectorAll('.tg_head_peer_info');
+            if (membersList.length < 1) {
+                setTimeout(function() {
+                    openMembersList();
+                }, 500)
             } else {
-                // saveFile(Array.from(loginCollection).join(' '), `Logins from telegram chat ${chatName}.txt`, 'text/plain');
-                saveFile(Array.from(loginMap, ([key, value]) => `\n ${key} ${value} `), `Logins with names from telegram chat ${chatName}.txt`, 'text/plain');
-                return;
+                console.dir(membersList[0].parentNode);
+                membersList[0].parentNode.click();
+                getMemberLink();
+            }
+
+        }
+
+        function getMemberLink() {
+            members = document.querySelectorAll('a.md_modal_list_peer_name');
+            if (members.length > 0) {
+
+                if (members.length > loginCollection.size) {
+                    members[parseInt(loginCollection.size)].click();
+                    getMemberLogin();
+                } else {
+                    loginMap = formatLoginMap(loginMap);
+                    timeEnd = new Date().getTime();
+                    fullTime = timeEnd - timeStart;
+                    // saveFile(Array.from(loginCollection).join(' '), `Logins from telegram chat ${chatName}.txt`, 'text/plain');
+                    let data = `{ ${Array.from(loginMap, ([key, value]) => `\n "${key}":"${value}" `)}\n}`
+                    saveFile(data , `Logins with names from telegram chat ${chatName}, Full time-${fullTime/1000}sec.txt`, 'text/plain');
+                    return;
             };
         } else {
             setTimeout(function() {
@@ -115,6 +122,14 @@ window.onload = function() {
         setTimeout(function() {
             getMemberLink();
         }, 100)
+    }
+
+    function formatLoginMap(map) {
+        let newMap = new Map();
+        for (let [key, val] of map) {
+            newMap.set(key.replace(/<span(.*)>/g, ''), val);
+        }
+        return newMap;
     }
 
 
