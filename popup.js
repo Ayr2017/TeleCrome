336f7b351c1a3069bae5a9fe3403b7bc3e656f8f
Ayrat Asfandiyarov <ayrat.2013@ya.ru>
2019-08-23 16:46:12 +0300

    передача массива адресов

--- popup.js
+++ popup.js
@@ -4,15 +4,30 @@ var sendmsgbtn = document.querySelector('#sendmsgbtn');
 var stopmsgbtn = document.querySelector('#stopmsgbtn');
 var infobtn = document.querySelector('#infobtn');
 var display = document.querySelector('#display');
+var fileUpload = document.querySelector('#file-upload');
+var sendfile = document.querySelector('#sendfile');
+var isSend;
+var addressCollectionAsText;
+var addressCollectionAsMap = new Map();
+
 
 ws.open = () => setStatus('ONLINE');
 ws.close = () => setStatus('DISCONECTED');
-ws.onmessage = response => writeInfo(response.data);
+ws.onmessage = response => {
+    writeInfo(response.data);
+    if (response.data == 'startSendingMessages') {
+        localStorage.setItem('isSend', true);
+        sendmsgbtn.classList.add('active');
+    } else if (response.data == 'stopSendingMessages') {
+        localStorage.setItem('isSend', false);
+        sendmsgbtn.classList.remove('active');
+    }
+};
 
 
 
 function getInfo() {
-    // ws.send('startSendingMessages');
+    ws.send('i');
 }
 
 function startSendingMessages() {
@@ -31,6 +46,49 @@ function writeInfo(value) {
     display.innerHTML = value;
 }
 
+function checkLocalStorageSendFlag() {
+    isSend = localStorage.getItem('isSend');
+    switch (isSend) {
+        case 'true':
+            sendmsgbtn.classList.add('active');
+            break;
+        case 'false':
+            sendmsgbtn.classList.remove('active');
+            break;
+    }
+}
+
 sendmsgbtn.addEventListener('click', startSendingMessages);
 stopmsgbtn.addEventListener('click', stopSendingMessages);
 infobtn.addEventListener('click', getInfo);
+sendfile.addEventListener('click', sendFileToServer);
+checkLocalStorageSendFlag();
+
+
+fileUpload.addEventListener('change', function() {
+    readText(this);
+});
+var reader = new FileReader();
+
+function readText(that) {
+    if (that.files && that.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var output = e.target.result;
+            output = output.trim();
+            addressCollectionAsText = output;
+            output = output.split(",");
+            for (let val of output) {
+                let v = val.split('@');
+                addressCollectionAsMap.set(v[0].trim(), '@' + v[1].trim())
+            }
+
+        };
+        reader.readAsText(that.files[0]);
+    }
+}
+
+function sendFileToServer() {
+    addressCollectionAsMap = JSON.stringify([...addressCollectionAsMap]);
+    ws.send(addressCollectionAsMap);
+}
--- script.js
+++ script.js
@@ -1,53 +1,3 @@
-// setInterval(() => { testWebSocket(); }, 1000); {
-
-//     var wsUri = "wss://echo.websocket.org/";
-//     var output;
-
-//     function init() {
-//         output = document.getElementById("output");
-//         testWebSocket();
-//     }
-
-//     function testWebSocket() {
-//         websocket = new WebSocket(wsUri);
-//         websocket.onopen = function(evt) { onOpen(evt) };
-//         websocket.onclose = function(evt) { onClose(evt) };
-//         websocket.onmessage = function(evt) { onMessage(evt) };
-//         websocket.onerror = function(evt) { onError(evt) };
-//     }
-
-//     function onOpen(evt) {
-//         writeToScreen("CONNECTED");
-//         doSend("WebSocket rocks");
-//     }
-
-//     function onClose(evt) {
-//         writeToScreen("DISCONNECTED");
-//     }
-
-//     function onMessage(evt) {
-//         writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
-//         websocket.close();
-//     }
-
-//     function onError(evt) {
-//         writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-//     }
-
-//     function doSend(message) {
-//         writeToScreen("SENT: " + message);
-//         websocket.send(message);
-//     }
-
-//     function writeToScreen(message) {
-//         console.log(message)
-//     }
-
-//     window.addEventListener("load", init, false);
-
-
-// }
-
 const ws = new WebSocket('ws://localhost:3000');
 
 ws.open = () => setStatus('ONLINE');
@@ -60,10 +10,13 @@ function messageProcessor(msg) {
         localStorage.setItem('isSend', true);
     } else if (msg == 'stopSendingMessages') {
         localStorage.setItem('isSend', false);
+    } else if (msg[0] == '[') {
+        localStorage.setItem('addressMap', msg);
     }
 }
+sendMessages();
 
-
-function writeInfo(value) {
-    console.log(value);
+function sendMessages() {
+    var isSend = localStorage.getItem('isSend');
+    console.log(isSend);
 }
