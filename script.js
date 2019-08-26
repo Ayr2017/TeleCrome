346abcef4ea3c41ab8bb40ec26991bdbd331152b
Ayrat Asfandiyarov <ayrat.2013@ya.ru>
2019-08-26 18:04:06 +0300

    add pop()

--- script.js
+++ script.js
@@ -18,5 +18,17 @@ sendMessages();
 
 function sendMessages() {
     var isSend = localStorage.getItem('isSend');
+    var addressMap = localStorage.getItem('addressMap');
+    var addressArray = JSON.parse(addressMap);
     console.log(isSend);
+    console.log(addressArray.pop());
+    console.log(addressArray);
+    console.log(new Map(addressArray));
+    var x = new Map(addressArray);
+    console.log(JSON.stringify([...x]));
+    localStorage.setItem('addressMap', JSON.stringify([...x]));
+}
+
+function writeToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
 }
